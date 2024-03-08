a4ad016b5041b7cab2fb3733389effdae9609c5e
RishiTripathi1 <114870015+RishiTripathi1@users.noreply.github.com>
2024-03-08 18:35:02 +0530

    Add files via upload

---
+++ A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Koi bhi prime number ke liye 2 and p-1 possible answer hai hamesha
+*/
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+
+        int p;
+        cin>>p;
+
+        cout<<2<<" "<<p-1<<"\n";
+    }
+
+}
