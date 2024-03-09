a98efe3e73252c5c8d635dee4085aa7a5795c892
RishiTripathi1 <114870015+RishiTripathi1@users.noreply.github.com>
2024-03-10 00:15:13 +0530

    Add files via upload

---
+++ A_Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Agar 2 se divisible hoega to do even me batega 
+special case 2 (1,1)
+*/
+
+int main(){
+
+        int n;
+        cin>>n;
+        if(n==2) cout<<"NO";
+        else if(n%2==0) cout<<"YES";
+        else cout<<"NO";
+
+}
